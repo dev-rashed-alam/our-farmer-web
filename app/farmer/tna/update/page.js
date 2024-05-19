044cf331f6db5e1caa87895434bb935600715bd0
dev-rashed-alam <dev.rashedalam@gmail.com>
2024-05-19 22:05:08 +0600

    Working with tna

--- app/farmer/tna/update/page.js
+++ app/farmer/tna/update/page.js
@@ -1,92 +1,145 @@
-import React from 'react';
+"use client"
+import React, {useState} from 'react';
 import '@/public/styles/farmer/Table.css';
 
 const scheduleData = [
     {
+        id: 1,
         phase: 'Land Preparation',
         activities: ['Plowing', 'Harrowing', 'Leveling'],
-        responsible: 'John Doe, Farm Workers',
+        remarks: 'John Doe, Farm Workers',
         startDate: 'June 1',
         endDate: 'June 14'
     },
     {
+        id: 2,
         phase: 'Seed Selection & Treatment',
         activities: ['Select seeds', 'Treat seeds'],
-        responsible: 'John Doe',
+        remarks: 'John Doe',
         startDate: 'June 15',
         endDate: 'June 16'
     },
     {
+        id: 3,
         phase: 'Sowing',
         activities: ['Seeding or transplanting'],
-        responsible: 'John Doe, Farm Workers',
+        remarks: 'John Doe, Farm Workers',
         startDate: 'June 17',
         endDate: 'June 23'
     },
     {
+        id: 4,
         phase: 'Water Management',
         activities: ['Irrigation maintenance'],
-        responsible: 'John Doe',
+        remarks: 'John Doe',
         startDate: 'June 24',
         endDate: 'Continuous'
     },
     {
+        id: 5,
         phase: 'Fertilization',
         activities: ['Basal', 'Tillering', 'Panicle Init.'],
-        responsible: 'John Doe',
+        remarks: 'John Doe',
         startDate: 'June 24',
         endDate: 'July 15'
     },
     {
+        id: 6,
         phase: 'Weed Management',
         activities: ['Manual weeding', 'Herbicide app.'],
-        responsible: 'John Doe, Farm Workers',
+        remarks: 'John Doe, Farm Workers',
         startDate: 'July 1',
         endDate: 'July 7'
     },
     {
+        id: 7,
         phase: 'Pest & Disease Management',
         activities: ['Scouting', 'Pesticide app.'],
-        responsible: 'John Doe',
+        remarks: 'John Doe',
         startDate: 'Continuous',
         endDate: 'Continuous'
     },
     {
+        id: 8,
         phase: 'Monitoring & Maintenance',
         activities: ['Field inspections'],
-        responsible: 'John Doe',
+        remarks: 'John Doe',
         startDate: 'Continuous',
         endDate: 'Continuous'
     },
     {
+        id: 9,
         phase: 'Harvesting',
         activities: ['Cutting', 'Threshing', 'Drying'],
-        responsible: 'John Doe, Farm Workers',
+        remarks: 'John Doe, Farm Workers',
         startDate: 'October 1',
         endDate: 'October 7'
     },
     {
+        id: 10,
         phase: 'Post-Harvest Handling',
         activities: ['Cleaning', 'Drying', 'Storage'],
-        responsible: 'John Doe',
+        remarks: 'John Doe',
         startDate: 'October 8',
         endDate: 'October 21'
     },
 ];
 
 const ScheduleTable = () => {
-    const renderRowspan = (phase, activities, startDate, endDate) => {
-        return activities.map((activity, index) => (
-            <tr className="crud-table__row" key={`${phase}-${index}`}>
-                {index === 0 && <td className="crud-table__cell" rowSpan={activities.length}>
-                    {phase}
+    const [selectedField, setSelectedField] = useState({})
+    const handleInputChange = () => {
+
+    }
+
+    const isEditable = (type, id) => {
+        return type === selectedField.type && id === selectedField.id
+    }
+
+    const setIsEditable = (type, id) => {
+        if (type !== selectedField.type && id !== selectedField.id) {
+            setSelectedField({
+                type,
+                id
+            })
+        }
+    }
+
+    const renderRowspan = (data) => {
+        return data.activities.map((activity, index) => (
+            <tr className="crud-table__row" key={`${data.phase}-${index}`}>
+                {index === 0 && <td className="crud-table__cell" rowSpan={data.activities.length}>
+                    {data.phase}
                 </td>}
                 <td className="crud-table__cell">{activity}</td>
-                <td className="crud-table__cell">
-                    {startDate}
+                <td className="crud-table__cell" onClick={() => setIsEditable("startDate", data.id)}>
+                    {isEditable("startDate", data.id) ? <input
+                        type="date"
+                        id="startDate"
+                        className="form-control"
+                        name="startDate"
+                        value={data.startDate}
+                        onChange={handleInputChange}
+                    /> : data.startDate}
                 </td>
-                <td className="crud-table__cell">
-                    {endDate}
+                <td className="crud-table__cell" onClick={() => setIsEditable("endDate", data.id)}>
+                    {isEditable("endDate", data.id) ? <input
+                        type="date"
+                        id="endDate"
+                        className="form-control"
+                        name="endDate"
+                        value={data.endDate}
+                        onChange={handleInputChange}
+                    /> : data.endDate}
+                </td>
+                <td className="crud-table__cell" onClick={() => setIsEditable("remarks", data.id)}>
+                    {isEditable("remarks", data.id) ? <input
+                        type="text"
+                        id="remarks"
+                        className="form-control"
+                        name="remarks"
+                        value={data.remarks}
+                        onChange={handleInputChange}
+                    /> : data.remarks}
                 </td>
             </tr>
         ));
@@ -100,15 +153,11 @@ const ScheduleTable = () => {
                 <th className="crud-table__header-cell">Activities</th>
                 <th className="crud-table__header-cell">Start Date</th>
                 <th className="crud-table__header-cell">End Date</th>
+                <th className="crud-table__header-cell">Remarks</th>
             </tr>
             </thead>
             <tbody className="crud-table__body">
-            {scheduleData.map(({
-                                   phase,
-                                   activities,
-                                   startDate,
-                                   endDate
-                               }) => renderRowspan(phase, activities, startDate, endDate))}
+            {scheduleData.map((data) => renderRowspan(data))}
             </tbody>
         </table>
     );
