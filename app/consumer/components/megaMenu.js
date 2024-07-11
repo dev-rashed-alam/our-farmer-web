726d2378aa9e6bdcd8b308289823b004fea2fc85
dev-rashed-alam <dev.rashedalam@gmail.com>
2024-07-11 19:49:10 +0600

    Added mega menu

--- app/consumer/components/megaMenu.js
+++ app/consumer/components/megaMenu.js
@@ -6,6 +6,7 @@ import {findAllCategories} from "@/app/service/catalogService";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchAllProducts, fetchProductsFailure, fetchProductsSuccess} from "@/app/store/productAction";
 import {fetchAllCategories, fetchCategoryFailure, fetchCategorySuccess} from "@/app/store/categoryAction";
+
 const MegaMenu = () => {
     const dispatch = useDispatch();
 
@@ -23,35 +24,40 @@ const MegaMenu = () => {
 
     return (
         <Navbar expand="lg" className='nav-mega-menu'>
-            {/*<Container>*/}
-            {/*    <Navbar.Toggle aria-controls="basic-navbar-nav" />*/}
-            {/*    <Navbar.Collapse id="basic-navbar-nav">*/}
-            {/*        <Nav className="me-auto">*/}
-            {/*            <NavDropdown title={<span>All Categories*/}
-            {/*                <svg width="20" height="17" viewBox="0 0 16 17" fill="none" className="text-token-text-tertiary"><path d="M11.3346 7.83203L8.00131 11.1654L4.66797 7.83203" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>*/}
-            {/*            </span>} id="basic-nav-dropdown" className='show'>*/}
-            {/*                {categories.map(category => (*/}
-            {/*                    <NavDropdown.Item key={category.id} href={'/consumer/product/?category='+ category.id}>*/}
-            {/*                        {category.name}*/}
-            {/*                    </NavDropdown.Item>*/}
-            {/*                ))}*/}
-            {/*            </NavDropdown>*/}
-            {/*            <Nav.Link href={'/'} className='fw-bold'>Home</Nav.Link>*/}
-            {/*            <Nav.Link href="/consumer/product" className='fw-bold'>Shop*/}
-            {/*            </Nav.Link>*/}
-            {/*            <Nav.Link href="/consumer/product?category=2" className='fw-bold'>Fruits and Vegetables</Nav.Link>*/}
-            {/*            <Nav.Link href={"#"} className='fw-bold'><Link href={'/consumer/contact'} className='text-decoration-none text-body-secondary'> Contact</Link></Nav.Link>*/}
-            {/*        </Nav>*/}
-            {/*        <Nav className="ml-auto">*/}
-            {/*            <Nav.Link href={'/consumer/product?isTrending=1'} className="trending fw-bold">Trending Product</Nav.Link>*/}
-            {/*            <Nav.Link href="#" className="text-danger">Almost Finished*/}
-            {/*                <span className="sales">*/}
-            {/*                    Sales*/}
-            {/*                </span>*/}
-            {/*            </Nav.Link>*/}
-            {/*        </Nav>*/}
-            {/*    </Navbar.Collapse>*/}
-            {/*</Container>*/}
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title={<span>All Categories
+                            <svg width="20" height="17" viewBox="0 0 16 17" fill="none"
+                                 className="text-token-text-tertiary"><path
+                                d="M11.3346 7.83203L8.00131 11.1654L4.66797 7.83203" stroke="currentColor"
+                                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                        </span>} id="basic-nav-dropdown" className='show'>
+                            {categories.map(category => (
+                                <NavDropdown.Item key={category.id} href={'/consumer/product/?category=' + category.id}>
+                                    {category.name}
+                                </NavDropdown.Item>
+                            ))}
+                        </NavDropdown>
+                        <Link href={'/'} className='fw-bold'>Home</Link>
+                        <Link href="/consumer/product" className='fw-bold'>Shop
+                        </Link>
+                        <Link href="/consumer/product?category=2" className='fw-bold'>Fruits and Vegetables</Link>
+                        <Link href={'/consumer/contact'}
+                              className='text-decoration-none text-body-secondary fw-bold'> Contact</Link>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Link href={'/consumer/product?isTrending=1'} className="trending fw-bold">Trending
+                            Product</Link>
+                        <Link href="#" className="text-danger">Almost Finished
+                            <span className="sales">
+                                Sales
+                            </span>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
